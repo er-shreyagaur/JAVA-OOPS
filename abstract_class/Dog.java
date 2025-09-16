f425c7accabac7b7bd0322afb58a524cb093d4e9
shreya gaur <119043761+er-shreyagaur@users.noreply.github.com>
2025-09-16 23:13:09 +0530

    Delete abstract_class directory

--- abstract_class/Dog.java
+++
@@ -1,9 +0,0 @@
-package abstract_class;
-
-abstract class Dog extends Animal {
-	public abstract void breedInfo(); // Abstract Method
-
-	public void wagTail() { // Concrete Method
-		System.out.println("The dog is wagging its tail");
-	}
-}
